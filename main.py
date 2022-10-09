33ac8ec6289c43a6e9a7803d04567f61fc8867fe
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-10-09 22:50:19 +0000

    1

---
+++ main.py
@@ -0,0 +1,35 @@
+marcas_ropa=[Dsquared, Philip plein , Emporio Armani]   #lista 
+ 
+
+lista 
+materiales_ropa[algodon, lino, cuero]  #tupla 
+ 
+print(marcas_ropa) 
+print(tupla) 
+ 
+ 
+print("/n") 
+ 
+print(marcas_ropa[2:]) 
+print(materiales_ropa[1]) 
+ 
+print("/n") 
+ 
+print(materiales_ropa) 
+print(marcas_ropa) 
+marcas_ropa[2]=gretch 
+print(marcas_ropa) 
+print("las tuplas no son modificables") 
+ 
+print("/n") 
+ 
+print(len(marcas_ropa)) 
+print(len(material_ropa)) 
+
+
+
+
+
+
+
+
